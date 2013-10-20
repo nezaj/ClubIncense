820fd928e7e37fb053aa7228b0d699f6307796a6
nezaj <nezaj@server.fake>
2013-10-20 15:47:13 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClubIncense::Application.config.secret_token = '6699b9ac6ac6b6ae7c98ebd0ebbd7b8dc5adef1af83cb8d91d75a683dc304f56150b5981dd96522763fddace70c83c8a44791d805a14b3a2ca3d21a8e647ed03'
